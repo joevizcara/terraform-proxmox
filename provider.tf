b3f43aed1f0e99a95c0fa0fc1d7e09e05e90e326
Joe B <joevizcara@proton.me>
2025-07-28 05:43:29 +0800

    test

--- provider.tf
+++ provider.tf
@@ -2,11 +2,21 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "~>3.0.2-rc03"
+      version = "3.0.2-rc03"
     }
   }
 }
 
 provider "proxmox" {
-  # Configuration options
+  pm_api_url = "https://192.168.1.30:8006/api2/json"
+  pm_api_token_id = "tofu-user@pam!tofu-token"
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_log_enable       = true                                    # Enable logging for debugging
+  pm_debug            = true                                    # Enable debug mode for detailed logs
+}
+
+variable "pm_api_token_secret" {
+  description = "proxmox api token secret"
+  type = string
+  sensitive = true
 }
