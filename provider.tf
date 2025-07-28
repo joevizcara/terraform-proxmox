e5baa5c98326c6f8ce91ab1c36d29075ab2cd19b
Joe B <joevizcara@proton.me>
2025-07-28 08:57:34 +0800

    test

--- provider.tf
+++ provider.tf
@@ -8,11 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url = "https://192.168.1.30:8006/api2/json"
-  pm_api_token_id = "tofu-user@pam!tofu-token"
-  pm_api_token_secret = var.pm_api_token_secret
-  pm_log_enable       = true                                    # Enable logging for debugging
-  pm_debug            = true                                    # Enable debug mode for detailed logs
+  endpoint  = var.pm_api_url
+    api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
+    insecure  = true
 }
 
 # variable "pm_api_token_secret" {
