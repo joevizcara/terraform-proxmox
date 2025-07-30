835d515e383ae2911e44e20d8025f0c28b8680d5
Joe B <joevizcara@proton.me>
2025-07-30 16:35:25 +0800

    fawef

--- provider.tf
+++ provider.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "proxmox" {
   endpoint  = var.pm_api_url
-  api_token = "PVEAPIToken=${var.pm_api_token_id}=${var.pm_api_token_secret}"
+  api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
   insecure  = true
 }
-#
