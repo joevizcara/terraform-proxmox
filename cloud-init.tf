5924f7ea2aa516c609fd6943c1084aa26310c2d8
Joe B <joevizcara@proton.me>
2025-07-30 14:08:04 +0800

    .img

--- cloud-init.tf
+++ cloud-init.tf
@@ -43,7 +43,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "import"
   datastore_id = "local"
   node_name    = var.vm_node
-  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.tar.gz"
+  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   # need to rename the file to *.qcow2 to indicate the actual file format for import
   file_name = "tofu-noble-server-cloudimg-amd64.qcow2"
 }
