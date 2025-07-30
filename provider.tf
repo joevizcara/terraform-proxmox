4a25d2362220a2d2d5da1db1783f490bfaa1b59f
Joe B <joevizcara@proton.me>
2025-07-30 15:56:13 +0800

    var

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "proxmox" {
   endpoint  = var.pm_api_url
-  api_token = "${var.pm_api_token_id}=${pm_api_token_secret}"
+  api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
   insecure  = true
 }
