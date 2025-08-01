331717f44802fcbb4857228618d2463b4b94d670
Joe B <joevizcara@proton.me>
2025-08-02 01:55:29 +0800

    sshpass

--- provider.tf
+++ provider.tf
@@ -11,12 +11,8 @@ provider "proxmox" {
   endpoint  = var.pm_api_url
   api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
   insecure  = true
-  # username = "tofu-user@pam"
-  # password = "1"
 
   ssh {
     agent = true
-    # username = "tofu-user"
-    # password = "1"
   }
 }
