b8fedc195099ae02410675daa80746136ce6cdc0
Joe B <joevizcara@proton.me>
2025-08-02 12:18:39 +0800

    existence check

--- cloud-init.tf
+++ cloud-init.tf
@@ -1,21 +1,15 @@
 
-resource "null_resource" "check_file" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Replace with your Proxmox API or SSH command to check file existence
-      ssh root@${var.pm_node} "test -f /var/lib/vz/import/noble-server-cloudimg-amd64.qcow2 && echo 'exists' || echo 'non-existent'" > file_check.txt
-    EOT
-  }
+data "external" "file_check" {
+  program = ["bash", "-c", <<EOT
+    ssh root@${var.pm_node} "test -f /var/lib/vz/import/noble-server-cloudimg-amd64.qcow2 && echo '{\"exists\": true}' || echo '{\"exists\": false}'"
+  EOT
+  ]
 }
 
-# Read the result of the file check
-data "local_file" "file_check" {
-  filename   = "file_check.txt"
-  depends_on = [null_resource.check_file]
-}
+
 
 resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  count        = data.local_file.file_check.content == "non-existent" ? 1 : 0
+  count        = data.external.file_check.result.exists ? 0 : 1
   content_type = "import"
   datastore_id = "local"
   node_name    = var.pm_node
@@ -24,6 +18,50 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   # need to rename the file to *.qcow2 to indicate the actual file format for import
   file_name = "noble-server-cloudimg-amd64.qcow2"
 }
+
+
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name      = "ubuntu-ci"
+  node_name = var.pm_node
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image[0].id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 16
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
+    meta_data_file_id = proxmox_virtual_environment_file.meta_data_cloud_config.id
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
+
+
 
 
 data "local_file" "ssh_public_key" {
@@ -81,48 +119,7 @@ resource "proxmox_virtual_environment_file" "meta_data_cloud_config" {
 }
 
 
-resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-  name      = "ubuntu-ci"
-  node_name = var.pm_node
 
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-  }
-
-  memory {
-    dedicated = 2048
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image[0].id
-    interface    = "virtio0"
-    iothread     = true
-    discard      = "on"
-    size         = 16
-  }
-
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-
-    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
-    meta_data_file_id = proxmox_virtual_environment_file.meta_data_cloud_config.id
-
-  }
-
-  network_device {
-    bridge = "vmbr0"
-  }
-
-}
 
 output "vm_ipv4_address" {
   value = proxmox_virtual_environment_vm.ubuntu_vm.ipv4_addresses[1][0]
