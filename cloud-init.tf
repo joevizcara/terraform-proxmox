29ca06018577fd57c0f2624506f89c01714b367e
Joe B <joevizcara@proton.me>
2025-08-03 09:59:31 +0800

    var correction

--- cloud-init.tf
+++ cloud-init.tf
@@ -106,6 +106,6 @@ resource "proxmox_virtual_environment_file" "meta_data_cloud_config" {
   }
 }
 
-output "ipv4" {
+output "initialization" {
   value = proxmox_virtual_environment_vm.ubuntu_vm[address]
 }
